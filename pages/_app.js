ed78e75de787e4727d33a6ec464f1cb311210305
ngnam <nguyenvannam0411@gmail.com>
2022-02-07 23:26:29 +0700

    add google analystic

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Script from 'next/script'
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-C02PHC117F" strategy="lazyOnload" />
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-C02PHC117F');
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
