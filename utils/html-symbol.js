7cf612c18e111d471dbd8414c6e4eea53aecdc3f
ngnam <nguyenvannam0411@gmail.com>
2022-02-14 15:23:53 +0700

    commit

--- utils/html-symbol.js
+++ utils/html-symbol.js
@@ -3,9 +3,8 @@
  * @return {string}
  * @author https://www.htmlsymbols.xyz/unicode/U+00B0
  */
-const HtmlSymbol = {
+export const HtmlSymbol = {
     degreeSign: "°",
     degreeSignJSCODE: "\u00B0",
     degreeSignEscape: "%C2%B0",
 };
-export default HtmlSymbol;
