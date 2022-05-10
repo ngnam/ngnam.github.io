4c810d7134c129048ca4f8c8387ca2a1b99494d6
ngnam <nguyenvannam0411@gmail.com>
2022-05-10 22:42:43 +0700

    fix ủl

--- pages/index.js
+++ pages/index.js
@@ -170,10 +170,10 @@ export default function Home() {
                 This is an example React Monday CRM Clone application using a DataStax Astra DB free tier database.
               </p>
               <p className="text-xl text-center font-light py-2">
-                <Link href=" https://monday-crm-clone.herokuapp.com"><a target="_blank">React App</a></Link> 
+                <Link href="https://monday-crm-clone.herokuapp.com"><a target="_blank">Public React App DEMO</a></Link> 
               </p>
               <p className="text-xl text-center font-light py-2">
-                <Link href="https://monday-crm-clone-api.herokuapp.com"><a target="_blank">API DEMO</a></Link> 
+                <Link href="https://monday-crm-clone-api.herokuapp.com"><a target="_blank">Public Monday API DEMO</a></Link> 
               </p>
             </div>
 
